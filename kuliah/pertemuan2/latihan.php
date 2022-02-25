3f5d6f6f1417ee94099d7425529a6125cfa33fc5
rejka permana <permanarejka@gmail.com>
2022-02-25 19:47:32 +0700

    -

---
+++ kuliah/pertemuan2/latihan.php
@@ -0,0 +1,35 @@
+<?php 
+// komentar ini digunakan untuk memberi catatan
+// beberapa catatan bisa disimpan dimanapun di baris manapun di 
+// kode ini
+// komentar untuk php
+
+// VARIABLE, untuk menyimpan nilai
+// bisa itu angka(integer), tulisan(string), boolean(true / false)
+
+// syarat nama bebas, tidak boleh ada spasi
+// boleh ada angka tapi tidak di awal
+$nama = 'Muhammad Rejka Permana';
+$npm = 213040098;
+
+echo $nama;
+echo '<hr>';
+// OPERATOR
+// Aritmatika
+// +,-,*,/,%
+echo 10 + 10;
+echo '<br>';
+echo 10 % 3;
+echo '<hr>';
+
+// COMPARATION / PEMBAGI
+// <, >, <=, >=, ==, !=
+echo 10 < 2; // false / null
+
+// Identitas
+// ===, !==
+
+// Operator Increment & Decrement
+// ++, --, +=, -=
+
+?>
