b54049f5e0e8a9db37f44ff475c4ca7f90a55b32
rejka permana <permanarejka@gmail.com>
2022-03-10 16:03:59 +0700

    pertemuan 5

---
+++ kuliah/pertemuan5/latihan2.php
@@ -0,0 +1,11 @@
+<?php
+	// Array multidimensi
+	// Array di dalam array
+	$mahasiswa = [
+		["M Rejka Permana","213040098","permanarejka@gmail.com","Teknik Informatika"],
+		["M Rejki Pratama","213040099","pratamarejki@gmail.com","Teknik Informatika"]
+	];
+	echo $mahasiswa[0][1];\
+	echo
+
+?>
